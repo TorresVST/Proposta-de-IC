efe2c1cb630b4cc057d20727d33118d5acb965e3
Rafael Torres <rafaeltorres.2020@alunos.utfpr.edu.br>
2025-10-20 21:33:13 -0300

    Update videoAnalise.py

--- videoAnalise.py
+++ videoAnalise.py
@@ -8,13 +8,20 @@ if not cap.isOpened():
 else:
     print("Vídeo aberto com sucesso.")
 
-ret, frame = cap.read()
+frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+fps = cap.get(cv2.CAP_PROP_FPS)
+print(f"Número total de frames: {frame_count}")
 
-if ret:
-    cv2.imshow("Primeiro Frame", frame)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-else:
-    print("Erro ao ler o primeiro frame do vídeo.")
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Fim do vídeo ou erro ao ler o frame.")
+        break
+
+    cv2.imshow("Frame do Vídeo", frame)
+
+    if cv2.waitKey(int(1000 / fps)) & 0xFF == ord('q'):
+        break
 
 cap.release()
+cv2.destroyAllWindows()
