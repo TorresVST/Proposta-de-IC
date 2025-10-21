ed1b5782997fc856df8932445d44d73dde2f6b3e
Rafael Torres <rafaeltorres.2020@alunos.utfpr.edu.br>
2025-10-20 21:36:02 -0300

    Update videoAnalise.py

--- videoAnalise.py
+++ videoAnalise.py
@@ -1,24 +1,19 @@
 import cv2
 
 cap = cv2.VideoCapture("C:/Users/rafae/Desktop/UTFPR/Extensão/IC/Esportes Python/Proposta-de-IC/Passes cortados/24-1.mp4")
-
 if not cap.isOpened():
-    print("Erro ao abrir o vídeo.")
-    exit()
-else:
-    print("Vídeo aberto com sucesso.")
+    print("Erro ao abrir o vídeo."); exit()
 
-frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 fps = cap.get(cv2.CAP_PROP_FPS)
-print(f"Número total de frames: {frame_count}")
+scale = 0.5  
 
 while True:
     ret, frame = cap.read()
     if not ret:
-        print("Fim do vídeo ou erro ao ler o frame.")
         break
 
-    cv2.imshow("Frame do Vídeo", frame)
+    resized = cv2.resize(frame, (0, 0), fx=scale, fy=scale, interpolation=cv2.INTER_AREA)
+    cv2.imshow("Video", resized)
 
     if cv2.waitKey(int(1000 / fps)) & 0xFF == ord('q'):
         break
